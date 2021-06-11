abc1849a931099542d3f14e105681deb52a804ac
Juliana Fernandez <julianafernandezlima@hotmail.com>
2021-06-11 18:24:30 -0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+function video() {
+    window.open("https://www.youtube.com/watch?v=v4K32z-gxcw");
+}
+
+function info() {
+    window.open("https://www.handmaidsbrasil.com/");
+}
